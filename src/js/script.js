e2e8754ad7f6df558eeef640a1fa2d1277a92fef
m-uson <usonibrahimovic@gmai.com>
2023-01-15 22:37:17 +0600

    added new section

--- src/js/script.js
+++ src/js/script.js
@@ -4,24 +4,24 @@ const navActive = document.getElementById("nav-active");
 const ul = document.querySelectorAll("li a");
 const body = document.querySelector("body");
 
-navBurger.addEventListener("click", function() {
-    navBurger.classList.toggle("active");
-    navActive.classList.toggle("menu-active");
-    body.classList.toggle("body-lock");
+navBurger.addEventListener("click", function () {
+	navBurger.classList.toggle("active");
+	navActive.classList.toggle("menu-active");
+	body.classList.toggle("body-lock");
 });
 
 ul.forEach((a) => {
-    a.addEventListener("click", function() {
-        navBurger.classList.remove("active");
-        navActive.classList.remove("menu-active");
-    });
+	a.addEventListener("click", function () {
+		navBurger.classList.remove("active");
+		navActive.classList.remove("menu-active");
+	});
 });
 
 activeItems.forEach((a) => {
-    a.addEventListener("click", function() {
-        activeItems.forEach((item) => {
-            item.classList.remove("is-active");
-        });
-        a.classList.add("is-active");
-    });
+	a.addEventListener("click", function () {
+		activeItems.forEach((item) => {
+			item.classList.remove("is-active");
+		});
+		a.classList.add("is-active");
+	});
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./**/*.html"],
-    theme: {
-        extend: {
-            colors: {
-                "color-b": "#191919",
-                "c-blue": "#118DA8 ",
-            },
-            fontFamily: {
-                f: ["DM Sans", "sans-serif"],
-            },
-        },
-        screens: {
-            "3xl": { max: "2660px" },
-            // => @media (max-width: 1660px) { ... }
+	content: ["./**/*.html"],
+	theme: {
+		extend: {
+			colors: {
+				"color-b": "#191919",
+				"c-blue": "#118DA8 ",
+			},
+			fontFamily: {
+				f: ["DM Sans", "sans-serif"],
+			},
+		},
+		screens: {
+			"3xl": { max: "2660px" },
+			// => @media (max-width: 1660px) { ... }
 
-            "2xl": { max: "17535px" },
-            // => @media (max-width: 1535px) { ... }
+			"2xl": { max: "17535px" },
+			// => @media (max-width: 1535px) { ... }
 
-            xl: { max: "1279px" },
-            // => @media (max-width: 1279px) { ... }
+			xl: { max: "1279px" },
+			// => @media (max-width: 1279px) { ... }
 
-            lg: { max: "1023px" },
-            // => @media (max-width: 1023px) { ... }
+			lg: { max: "1024px" },
+			// => @media (max-width: 1023px) { ... }
 
-            md: { max: "767px" },
-            // => @media (max-width: 767px) { ... }
+			md: { max: "800px" },
+			// => @media (max-width: 800px) { ... }
 
-            sm: { max: "640px" },
-            // => @media (max-width: 640px) { ... }
-        },
-        container: {
-            padding: "10px",
-            center: true,
-        },
-    },
-    plugins: [],
+			sm: { max: "640px" },
+			// => @media (max-width: 640px) { ... }
+		},
+		container: {
+			padding: "10px",
+			overflow: "hidden",
+			center: true,
+		},
+	},
+	plugins: [],
 };
